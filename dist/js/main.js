c1368247c555cdcdec39b465344e833cc70b7f6e
sofia durkan <sofiadurkan@gmail.com>
2021-07-29 23:38:15 -0500

    menu button transition added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,36 +1,34 @@
-//select dom items
 
-const menuBtn =document.querySelector('.menu-btn');
-const menu =document.querySelector('.menu');
-const menuNav =document.querySelector('.menu-nav');
-const menuBranding =document.querySelector('.menu-branding');
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-
-//set initial state of Menu
-
-
+// Set Initial State Of Menu
 let showMenu = false;
-menuBtn.addEventListener('click',toggleMenu);
+
+menuBtn.addEventListener('click', toggleMenu);
+
 function toggleMenu() {
-    if(!showMenu){
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
 
-        //set Menu state
-        showMenu = true;
-    }else{
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
 
-        //set menu state
-        showMenu = false;
-
-    }
+    // Set Menu State
+    showMenu = false;
+  }
 }
